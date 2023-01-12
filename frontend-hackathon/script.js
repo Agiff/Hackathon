bc3017bcb74497830485aa0e73670b2fea6cf9e1
asbon-situmorang <situmorang.asbon@gmail.com>
2023-01-12 20:54:37 +0700

    nambah fitur login dan sign up

--- frontend-hackathon/script.js
+++ frontend-hackathon/script.js
@@ -17,9 +17,6 @@ function myFunction(){ // fungsi live filter
   let test = document.getElementsByClassName("box-container")
   let container = test[0].children
   let filter = document.getElementById("search-box").value.toUpperCase()
-  // console.log(filter)
-  // let arrItem = container.getElementsByClassName("box")
-  // console.log(container[0])
           
   for (let i = 0; i < container.length; i++) {
     const itemList = container[i].getElementsByTagName("h3")
@@ -34,3 +31,33 @@ function myFunction(){ // fungsi live filter
 
   }
 } 
+
+// fitur pop-up registrasi form dan login
+var modalReg = document.getElementById("ModalReg");
+var regBtn = document.getElementById("reg-button");
+var closeReg = document.getElementById("close-reg");
+
+regBtn.onclick = function() {
+  modalReg.style.display = "block";
+}
+
+closeReg.onclick = function() {
+  modalReg.style.display = "none";
+}
+
+var modalLog = document.getElementById("ModalLog");
+var logBtn = document.getElementById("log-button");
+var closeLog = document.getElementById("close-log")
+
+logBtn.onclick = function() {
+  modalLog.style.display = "block";
+}
+closeLog.onclick = function() {
+  modalLog.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
